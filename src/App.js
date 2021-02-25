7919f8d1eaa0c9308e0ddad82fe29c9178e1c3ca
Winter Dresden <winterdresden@Winters-MacBook-Air.local>
2021-02-25 15:29:14 -0700

    firs push

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useState } from 'react'
+// import Button from './components/Button'
 
 import Map from './components/Map'
 
@@ -8,10 +9,33 @@ function App() {
   const [center, setCenter] = useState([43.88, -72.7317])
 
   return (
-    <div>
-      <Map center={center} />
+    <div style={{textAlign:'center'}} >
+      <h1>GeoVermont</h1>
+      <div style={{height: '7vh',border: '1px solid black', textAlign: 'center', }}>
+       
+        <a href="https://google.com"><div>About</div></a>
+        <a href="pages/highscore.html"><div>High Score</div></a>
+        <div id="text">score / lat / long / county / town</div>
+      </div> 
+      <div style={{alignSelf:'right'}}>
+        <Map center={center}/>
+      </div>
+      <div id="menu">
+        
+        <button>N</button> 
+        <button>E</button> 
+        <button>W</button> 
+        <button>S</button>
+      </div>
+      {/* <Button /> */}
+      <button>Quit</button>
+      <button>Guess</button>
+      <button onClick={() => setCenter([41, -72])}>Click Me</button>
     </div>
   );
 }
 
 export default App;
+
+// onClick={moveMarker}
+// [43.88, -72.7317]
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,18 +1,39 @@
 import { MapContainer, TileLayer, Polygon, Marker, Polyline } from "react-leaflet";
-import borderData from "../data/border";
+import Border from '../data/border';
+import L from 'leaflet';
+import leafletPip from "leafletPip";
 
 function Map(props) {
   let vtOutline = borderData.geometry.coordinates[0].map(coords => [coords[1], coords[0]])
+  
+  
+// make a polygon
+  let polygon = L.vtOutline
+  console.log(polygon)
+
+// max and mins
+let x_min = -73.42613118833583;
+let x_max = -71.51022535353107;
+let y_min = 42.730315121762715;
+let y_max = 45.00541896831666;
+
+// generating randoms
+let lat = y_min + (Math.random() * (y_max - y_min));
+let lng = x_min + (Math.random() * (x_max - x_min));
+ 
+console.log(leafletPip.pointInLayer([lng,lat], vtOutline))
 
   return (
+
+    <div>
     <MapContainer
       center={props.center}
       zoom={8}
-      scrollWheelZoom={false}
+      scrollWheelZoom={true}
       doubleClickZoom={false}
       zoomControl={false}
       touchZoom={false}
-      style={{ height: "600px", width: "600px" }}
+      style={{ height: "600px", width: "600px", textAlign: "center" }}
     >
       <TileLayer
         url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
@@ -21,10 +42,49 @@ function Map(props) {
       <Marker position={props.center} />
       <Polygon
         positions={vtOutline}
-        pathOptions={{ color: "orange", fillOpacity: 0 }}
+        pathOptions={{ color: "pink", fillOpacity: 0 }}
       />
     </MapContainer>
+
+    </div>
+
   );
 }
 
+
 export default Map;
+
+// // define the function
+// randomPointInPoly = function(polygon) {
+//   var bounds = polygon.getBounds(); 
+//   var x_min  = bounds.getEast();
+//   var x_max  = bounds.getWest();
+//   var y_min  = bounds.getSouth();
+//   var y_max  = bounds.getNorth();
+
+//   var lat = y_min + (Math.random() * (y_max - y_min));
+//   var lng = x_min + (Math.random() * (x_max - x_min));
+
+//   var point  = turf.point([lng, lat]);
+//   var poly   = polygon.toGeoJSON();
+//   var inside = turf.inside(point, poly);
+
+//   if (inside) {
+//       return point
+//   } else {
+//       return randomPointInPoly(polygon)
+//   }
+// }
+
+// // create a poly
+// var polygon = L.polygon([
+//   [51.509, -0.08],
+//   [51.503, -0.06],
+//   [51.51, -0.047]
+// ]).addTo(map);
+
+// // get a geojson point from the function
+// var point = randomPointInPoly(polygon);
+
+// // .. or add it to the map directly from the result
+// L.geoJson(randomPointInPoly(polygon)).addTo(map);
